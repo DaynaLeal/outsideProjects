52067483b851f742ab437e62f0d2e73ffa2d494c
Dayna Leal <dayna.r.leal@gmail.com>
2019-11-25 16:58:41 -0600

    js practice edabit

---
+++ JavaScript/jsIntro.js
@@ -0,0 +1,37 @@
+//1. returned input squared
+function squared(b) {
+    return b*b;
+}
+console.log(squared(5));
+
+
+
+//2. return area of a triangle
+function triArea(base, height) {
+    return (base * height) / 2;
+}
+
+
+
+//3. return seconds in #hours
+function howManySeconds(hours) {
+    return (hours * 60 * 60);
+}
+
+
+
+//4. return remainder of division of two numbers
+function remainder(x, y) {
+    return (x % y);
+}
+
+
+
+//return total minutes from #hours and minutes
+function convert(hours, minutes) {
+    return (hours * 60 * 60) + (minutes * 60);
+}
+
+//Create a function that finds the maximum range of a triangles third edge.
+//hint: (side1 + side2) - 1 = maximum range of third edge.
+
