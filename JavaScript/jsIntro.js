ceea8d9d766a2a466346aa6bdbb3cf6e8c85910d
Dayna Leal <dayna.r.leal@gmail.com>
2019-11-26 15:59:50 -0600

    edabit practice

--- JavaScript/jsIntro.js
+++ JavaScript/jsIntro.js
@@ -27,11 +27,93 @@ function remainder(x, y) {
 
 
 
-//return total minutes from #hours and minutes
+//5. return total minutes from #hours and minutes
 function convert(hours, minutes) {
     return (hours * 60 * 60) + (minutes * 60);
 }
 
-//Create a function that finds the maximum range of a triangles third edge.
-//hint: (side1 + side2) - 1 = maximum range of third edge.
 
+
+//6. Create a function that finds the maximum range of a triangles third edge.
+//hint: (side1 + side2) - 1 = maximum range of third edge.
+function nextEdge(side1, side2) {
+    return ((side1 + side2) - 1);
+}
+
+
+//7. Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+function lessThanOrEqualToZero(num) {
+    if (num <= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+//8. verify the equality of two different given parameters: a and b. Both the value and the type of parameters need to be tested for to have an equality matching.
+function checkEquality(a, b) {
+    if (a === b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+//9. Create a function that returns true if an integer is divisible by 5, and false otherwise.
+function divisibleByFive(n) {
+    if (n % 5 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+//10. In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+// chickens = 2 legs
+// cows = 4 legs
+// pigs = 4 legs
+// The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+function animals(chickens, cows, pigs) {
+    return (chickens * 2) + (cows * 4) + (pigs * 4);
+}
+
+
+//11. Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+function addition(num) {
+    return (++num);
+}
+
+
+
+//12. Create a function that takes two strings as arguments and return either true or false depending on whether the total number of characters in the first string is equal to the total number of characters in the second string.
+function comp(str1, str2) {
+    if (str1.length == str2.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+//13. Write a function that returns the length of a string. Make your function recursive.
+function length(str) {
+    return str.length;
+}
+
+
+//14. Create a function that takes in three arguments (prob, prize, pay) and returns true if prob * prize > pay; otherwise return false.
+// To illustrate, profitableGamble(0.2, 50, 9) should yield true, since the net profit is 1 (0.2 * 50 - 9), and 1 > 0.
+function profitableGamble(prob, prize, pay) {
+    if ((prob * prize) > pay) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+//15.
