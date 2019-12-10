4e96367c8b2dd688b0e5b45ee05db77cb551d5d1
Dayna Leal <dayna.r.leal@gmail.com>
2019-12-10 09:23:24 -0600

    js conditionals assessment review, almost done

--- JavaScript/edabit.js
+++ JavaScript/edabit.js
@@ -197,4 +197,15 @@ function numberArgs(arr) {
 }
 
 
-//26.
+//26.Create a function that takes a base number and an exponent number and returns the calculation.
+function calculateExponent(num, exp) {
+    return Math.pow(num, exp);
+}
+
+//27.Imagine you run a website that presents users with different coding challenges in levels Easy, Medium, and Hard, where users get points for completing challenges. An Easy challenge is worth 5 points, a Medium challenge is worth 10 points, and a Hard challenge is worth 20 points.
+//
+// Create a function that takes in the number of each challenge level a user has played and calculates the user's total number of points. Keep in mind that a user cannot complete negative challenges, so the function should return the string "invalid" if any of the passed parameters are negative.
+
+
+
+
