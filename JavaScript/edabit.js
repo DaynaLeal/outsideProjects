d3e8fa849bcf6933f149107996552dc55399181d
Dayna Leal <dayna.r.leal@gmail.com>
2020-05-11 23:19:53 -0500

    edabit num to month solution and refactored

--- JavaScript/edabit.js
+++ JavaScript/edabit.js
@@ -236,6 +236,10 @@ function minMax(arr) {
 //
 // }
 
+
+
+
+//-------------------------------------------------------------------------------------------------------------------
 // 2. Broken Bridge: Create a function which validates whether a bridge is safe to walk on (i.e. has no gaps in it to fall through).
 //isSafeBridge("####") ➞ true
 // isSafeBridge("## ####") ➞ false
@@ -247,6 +251,10 @@ function isSafeBridge(s){
 // console.log(isSafeBridge("## ####"));
 // console.log(isSafeBridge("####"));
 
+
+
+
+//-------------------------------------------------------------------------------------------------------------------
 //3. Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
 // example:
 // largestSwap(27) ➞ false
@@ -272,6 +280,61 @@ function largestSwap(num) {
     // console.log(typeof numToString);
     return (num >= numToString);
 }
-console.log(largestSwap(15));
-console.log(largestSwap(45));
+// console.log(largestSwap(15));
+// console.log(largestSwap(45));
 
+
+
+//-------------------------------------------------------------------------------------------------------------------
+//4. Convert Number to Corresponding Month Name
+//Create a function that takes a number (from 1 to 12) and returns its corresponding month name as a string. For example, if you're given 3 as input, your function should return "March", because March is the 3rd month.
+//SWITCH CASE ANSWER
+//     function monthName(num) {
+//         switch (num){
+//             case 1:
+//                 return 'January';
+//                 break;
+//             case 2:
+//                 return 'February';
+//                 break;
+//             case 3:
+//                 return 'March';
+//                 break;
+//             case 4:
+//                 return 'April';
+//                 break;
+//             case 5:
+//                 return 'May';
+//                 break;
+//             case 6:
+//                 return 'June';
+//                 break;
+//             case 7:
+//                 return 'July';
+//                 break;
+//             case 8:
+//                 return 'August';
+//                 break;
+//             case 9:
+//                 return 'September';
+//                 break;
+//             case 10:
+//                 return 'October';
+//                 break;
+//             case 11:
+//                 return 'November';
+//                 break;
+//             case 12:
+//                 return 'December';
+//                 break;
+//         }
+//     }
+//REFACTORED ARRAY ANSWER
+function monthName(num) {
+    let numArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    for(let i = 0; i < numArr.length; i++){
+        if (num === (i + 1)){
+            return numArr[i];
+        }
+    }
+}
